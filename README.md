4d848e473f6a409abcfa7185072a25ad426eae5c
Engou <2251985371@qq.com>
2019-08-04 16:53:14 +0800

    v1.2.0 更新了： 1、更换gui库； 2、把HEXN移动到flash区，节省动态内存； 3、添加了u8glib库

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 - vscode
 - PlatformIO IDE for Arduino
 ## libary
-- GFX for  arduino;
+- GFX
 - SSD1306
 - IRremote
+- u8glib
 ### how to install libary
 - copy your library to `./lib`
 - add every library path to `included path` in vscode
@@ -14,3 +15,16 @@
 - oled screen with `I2c` protocol and `ssd1306` control chip
 - mini IR remote controller
 - IR revicer
+## RAM
+In the `./test`, The `main.cpp` is a project with ssd1306 library, because of high ram utilization(91.3%), I abandoned it. when I used u8glib, the utilization is 38.7%. In the `./src`, The `main.cpp` is a project with u8glib, according my compiler result, the date(ram) utilization is `38.7% (used 792 bytes from 2048 bytes)`, the program utilization is `54.1% (used 17450 bytes from 32256 bytes)`
+## usage
+- connect your oled and IR revicer to your arduino uno
+
+IR revicer:
+```
+GND:GND
+VCC:5V
+Vout:11
+```
+- edit the function `PageS` to new a page and it's id with u8g2
+- edit the function `keyChange` to new the rule of page switching
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,16 @@
 #include <Arduino.h>
-#include <Adafruit_GFX.h>//gui library for arduino    
-#include <Adafruit_SSD1306.h>  //oled control chip library
+#include <U8glib.h>
 #include <IRremote.h>   //IR receiver
-#define OLED_RESET 4 
-Adafruit_SSD1306 display(OLED_RESET);  
-long HEXN[21]={ //mini remote control key hex id
-0xFD00FF
-,0xFD807F
-,0xFD40BF
-,0xFD20DF
-,0xFDA05F
-,0xFD609F
-,0xFD10EF
-,0xFD906F
-,0xFD50AF
-,0xFD30CF
-,0xFDB04F
-,0xFD708F
-,0xFD08F7
-,0xFD8877
-,0xFD48B7
-,0xFD28D7
-,0xFDA857
-,0xFD6897
-,0xFD18E7
-,0xFD9867
-,0xFD58A7
+#include <avr/pgmspace.h>
+
+const unsigned long HEXN[21]={ //mini remote control key hex id
+ 0xFD00FF,0xFD807F,0xFD40BF
+,0xFD20DF,0xFDA05F,0xFD609F
+,0xFD10EF,0xFD906F,0xFD50AF
+,0xFD30CF,0xFDB04F,0xFD708F
+,0xFD08F7,0xFD8877,0xFD48B7
+,0xFD28D7,0xFDA857,0xFD6897
+,0xFD18E7,0xFD9867,0xFD58A7
 }; 
 
 String Name[21]={  //mini remote control key name
@@ -45,21 +30,38 @@ String Name[21]={  //mini remote control key name
 };
 int RECV_PIN = 11;   //IR revicer pin
 int D3 = 3;          //interrupt pin
-int controller=0;    // storage key hex id
+int D2 =2;           //interrupt pin
+long  controller=0;    // storage key hex id
 int page=1;         //page number now
 IRrecv irrecv(RECV_PIN); 
 decode_results results;
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI 
+
+void u8glibSet(){
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+}
+long NumToHex(int n){
+  return pgm_read_byte(&HEXN[n]);
+}
 
 void PageS(long n){              //show page with gui code
-  display.clearDisplay();        //clean the screen
-  display.setTextSize(1);       //set font size
-  display.setTextColor(WHITE);   //set font color
-  display.setCursor(0,0);     //set font position
-  if(n=1){
-    display.println("Init.");   //display information
-  }
-  else if(n=1.1){
-    display.println("yes!");
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  if(n==1)
+     u8g.drawStr( 0, 22, "Hello World!");
+  else if(n==1.1){
+     u8g.drawStr( 0, 22, "yes!");
   }
 }
 /*
@@ -78,19 +80,23 @@ long NameToHex(String N){
   }
 }*/
 void keyChange(){                     //interrupt function
-    if(page==1&&controller==HEXN[4]){    // you press playStop button when your screen is page 1
+    if(page==1&&controller==NumToHex(4)){    // you press playStop button when your screen is page 1
       page=1.1;                          //set page flag to 1.1
     }
-    if(page==1.1&&controller==HEXN[3]){  //your press playback button when your screen is page 1.1
+    if(page==1.1&&controller==NumToHex(3)){  //your press playback button when your screen is page 1.1
       page=1;                            //set page flag to 1
     }
 }
-void setup(){                              
+void valueChange(){
+
+}
+void setup(){         
+  u8glibSet();                      
   pinMode(D3,INPUT);                   //set d3 mode(d3 is interrupt pin)
   delay(500);                                    //I don't know why it is here
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);     //I don't know why it is here too
   irrecv.enableIRIn();                           //enable IR revicer 
-  attachInterrupt(1,keyChange,CHANGE);         //set interrupt pin,set interrupt function,set interrupt mode（Triggered when changing）
+  attachInterrupt(1,keyChange,CHANGE);         //set interrupt pin,set interrupt function,set interrupt mode(Triggered when changing)
+  attachInterrupt(0,valueChange,CHANGE); 
 }
 
 void loop() {                
