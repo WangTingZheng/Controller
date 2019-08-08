368f79b0ee2e43c33e5549fe9dbac6f81d7fcd82
Engou <2251985371@qq.com>
2019-08-08 19:21:50 +0800

    v1.4.1 更新了： 1、修正了readem文档； 2、修正了main.cpp; 3、添加了CI图标

--- README.md
+++ README.md
@@ -1,25 +1,20 @@
 # Controller for Arduino
-**I am sorry to telling you that I haven't tested this project in my arduino, but it has been passed builting in vscode with PlatformIO**
+[![Build Status](https://travis-ci.com/WangTingZheng/Controller.svg?branch=master)](https://travis-ci.com/WangTingZheng/Controller)
 # install
 - vscode
 - PlatformIO IDE for vscode
+# configure
+- run 
+```git
 
-# hardware
-- Arduino uno
-- oled screen with `I2c` protocol and `ssd1306` control chip
-- mini IR remote controller
-- IR revicer
-  
-# Something Important
-## about libary
-- GFX(not recomand)
-- SSD1306(together with GFX)
-- IRremote
-- u8glib(it's work)
-- u8g2(not work because of high ram utilization)
-### how to install libary
-- copy your library to `./lib`
-- add every library path to `included path` in vscode
+git clone https://github.com/wangtingzheng/controller.git
+```
+to clone my project to your computer.
+
+- open vscode 
+- open PlatformIO 
+- import my project
+- configure project setting
 
 open `.vscode/c_cpp_properties.json`, you can fond this:
 ```c
@@ -41,52 +36,58 @@ open `.vscode/c_cpp_properties.json`, you can fond this:
                 ""
             ],
 ```
-For example, you want to add u8g2 to your project's library(you have already copied the u8g2 folder to `./lib`), you should add `"my project path in your pc/controller/lib/U8g2"`,
+fix the location which is include `./lib`, this is the library folder.
+- press the platformio build under the vscode bar
+- if everything is ok, the building will be passed.
+
+# hardware
+- Arduino uno
+- oled screen with `I2c` protocol and `ssd1306` control chip(four pin)
+- mini IR remote controller 
+- IR reviver
+# connect
+oled:
+```
+GND-GND
+VCC-3.3V
+SCL-SCL
+SDA-SDA
+```
+mini IR remote controller :
+```
+No need to connection, it's Wireless.
+```
+IR reviver
+```
+GND-GND
+VCC-5V
+Vout-11
+```
+# function
+- some windows like drawer 
+- read values in the project in oled
+- modify values with IR controller
+
+![function.jpg](https://i.loli.net/2019/08/08/qrbTC4jdxlURVaP.jpg)
+# Something Important
+## about library
+- GFX(not recommend)
+- SSD1306(together with GFX)
+- IRremote
+- u8glib(it's work)
+- u8g2(not work because of high ram utilization)
+### how to install library
+- copy your library to `./lib`
+- add every library path to `included path` in vscode
 
 ## about RAM
-If you want use it in Arduino uno, I am sorry to telling you that your arduino may lost all usable ram becaue of some GUI library, I have already shou the ram utilization of different GUI library, you can choose what you want. if you have more powerful Arduino, everything is nothong.
+If you want use it in Arduino uno, I am sorry to telling you that your Arduino may lost all usable ram because of some GUI library, I have already show the ram utilization of different GUI library, you can choose what you want. if you have more powerful Arduino, everything is nothing.
 
 ## about `./test`
 The folder has three C++ source file, they are:
-### main_gxf.cpp:
-This is a project with ssd1306 library.
 
-because of high ram utilization(91.3%), I abandoned it.
-
-when I used u8glib ,the utilization is 38.7%.
-### main_u8g2.cpp:
-This is a project with u8g2 library(the more powerful u8glib)
-
-because of high ram utilization(104%), I abandoned it
-
-It's terrible, It seems that I should change my arduino
-
-when I used u8glib ,the utilization is 38.7%,it's ok.
-### main_u8g2lib.cpo:
-This is a project with u8glib library.
-
-because of high ram utilization(38.7%), I am using it noew.
-
-It has poor performance, but it's enough.
-
-The drawback is that the u8glib/utility/u8g_rot.c(line 48) has an warning
-
-The developer said that it can be ignored.
-
-https://github.com/olikraus/u8glib/issues/366
-
-# usage
-**Play attention: If vscode tells you it can not found some library, please detect my include path and add your project's.**
-- git clone to your comptuer
-- built this project with vscode(PlatformIO) to make should it's work on your comptuer
-- connect your oled and IR revicer to your arduino uno
-- not finshed, i will add entire step when i test it in my arduino
-
-IR revicer:
-```
-GND:GND
-VCC:5V
-Vout:11
-```
-- edit the function `PageS` to new a page and it's id with u8g2
-- edit the function `keyChange` to new the rule of page switching
+|                 | U8glib                                                       | U8g2                            | GXF                 |
+| --------------- | ------------------------------------------------------------ | ------------------------------- | ------------------- |
+| ram utilization | 38.7%                                                        | 104%                            | 91%                 |
+| usage           | :+1:                                                         | :no_entry_sign:                 | :fearful:           |
+| PS              | a little warning, but it' [ok](https://github.com/olikraus/u8glib/issues/366) | the  Upgraded version of u8glib | another GUI library |
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <IRremote.h>   //IR receiver
 #define line 15
 #define column 10
-#define maxKey 50
-long HEXN[21]={ //mini remote control key hex id
+#define maxKey 50   //the max length of the array which include the match you input
+long HEXN[21]={     //mini remote control key hex id
  0xFD00FF,0xFD807F,0xFD40BF
 ,0xFD20DF,0xFDA05F,0xFD609F
 ,0xFD10EF,0xFD906F,0xFD50AF
@@ -18,27 +18,17 @@ int key[maxKey+1];  //key[10] is flag
 
 int RECV_PIN = 11;   //IR revicer pin
 long  controller=0;    // storage key hex id
-int   controllerF=0;
-int speed=100;
-int flag;
-int xxx;
+int   speed=100;      //the value i want to modify
+int   flag;           //the flag target differernt page
+int   updateF;       //the updateValue's flag
 
 
 IRrecv irrecv(RECV_PIN); 
 decode_results results;
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI 
 
-int idToNu(long id){
-  for(int i=0;i<21;i++){
-    if(HEXN[i]==id){
-      return i;
-    }
-  }
-  return -1;
-}
 
-
-void u8glibSet(){
+void u8glibSet(){                                //oled screen init
   if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
     u8g.setColorIndex(255);     // white
   }
@@ -53,12 +43,12 @@ void u8glibSet(){
   }
 }
 
-void page_1(){
+void page_1(){                               
   u8g.setFont(u8g_font_unifont);
   u8g.drawStr(0, line*1, "May I help you?");
   u8g.drawStr(0, line*2, "1.modify speed.");
   u8g.drawStr(0 ,line*3, "2.read speed.");
-  key[maxKey]=0;
+  key[maxKey]=0;                             //reroad input data flag
 }
 
 void page_1_1_1(){
@@ -68,24 +58,24 @@ void page_1_2(){
   u8g.setFont(u8g_font_unifont);
   u8g.drawStr( 0, line*1, "the speed is:");
   u8g.setPrintPos(0, line*2);
-  u8g.print(speed);
+  u8g.print(speed);  
 }
 
 void draw_Input(int l,int c) {
   u8g.setFont(u8g_font_unifont);
-  if(l==1){
-    u8g.drawStr( 0, 30, " 4  5  6");
-    u8g.drawStr( 0, 45, " 7  8  9");
-    u8g.drawStr( 0, 60, " 0     .");
-    if(c==1){
-        u8g.drawStr( 0, 15, "#1  2  3");
-    }
-    else if(c==2){
-        u8g.drawStr( 0, 15, " 1 #2  3");
-    }
-    else if(c==3){
-        u8g.drawStr( 0, 15, " 1  2 #3");
-    }
+  if(l==1){                                        //if you want display "#" in line 1
+      u8g.drawStr( 0, 30, " 4  5  6");
+      u8g.drawStr( 0, 45, " 7  8  9");
+      u8g.drawStr( 0, 60, " 0     .");
+      if(c==1){                                 //if you want to display "# in font of column  1,it's number:1
+          u8g.drawStr( 0, 15, "#1  2  3");
+      }
+      else if(c==2){
+          u8g.drawStr( 0, 15, " 1 #2  3");
+      }
+      else if(c==3){
+          u8g.drawStr( 0, 15, " 1  2 #3");
+      }
   }
   else if(l==2){
     u8g.drawStr( 0, 15, " 1  2  3");
@@ -138,14 +128,15 @@ void draw_Input(int l,int c) {
     }
 }
 
-int thePower(int z){
+int thePower(int z){   //calculate the 10^z
   int temp=1;
   for(int q=0;q<z;q++){
     temp *=10;
   }
   return temp;
 }
-void updateValue(int add){
+
+void updateValue(int add){   //add one value behind the array "key"
   if(key[maxKey]!=maxKey){
     key[key[maxKey]]=add;
     key[maxKey]++;
@@ -153,13 +144,13 @@ void updateValue(int add){
     key[maxKey]=0;
   }
 }
-void display_Side(){
+void display_Side(){                    //display the side which we don't change it
     u8g.drawStr(80,15, "|Enter");
     u8g.drawStr(80,30, "|the ");
     u8g.drawStr(80,45, "|speed");
 }
 
-int findID(){
+int findID(){                      //find the id which you press now,according to the value of "controller"
   for(int l=0;l<21;l++){
       if(controller==HEXN[l]){
           return l;
@@ -181,45 +172,38 @@ void page_1_1(){
   int number_press;
   int x,y;
   int numberP=-1;  //the buuton you pressed id
-  if(controllerF==1){
-    numberP=findID();
-    display_Side();
-    if(numberP==9){
-        draw_Input(4,1);
-        updateValue(0);
+  numberP=findID();     //find the id of your pressed button
+  display_Side();      //display the side
+  if(numberP==9){       //if your pressed number "0"
+      draw_Input(4,1);  //draw the picture which the "#" in font of 0
+      updateValue(0);   //add 0 to the array key to modify in the reasonable time
+  }
+  else if(numberP==4){  //confirm
+      flag=4;            //the next page is page_1_1_1
+      modify(&speed);   //modify the value speed
+  }
+  else if(numberP==3){ //cansle
+        flag=1;        //next value is page_1
+  }
+  else if(numberP>11){  //if you pressed number 
+    number_press=numberP-11; //get the real number 
+    if(updateF==1) {updateValue(number_press); updateF=0;}
+    if((number_press%3)!=0){  //get the number's position
+        x=number_press/3+1;
+        y=number_press%3;
     }
-    else if(numberP==4){  //confirm
-        flag=4;
-        modify(&speed);
+    else {
+        x=number_press/3;
+        y=3;
     }
-    else if(numberP==3){ //cansle
-          flag=1;
-          key[maxKey]=0;
-    }
-    else if(numberP>11){
-      number_press=numberP-11;
-      if(xxx==1) {updateValue(number_press); xxx=0;}
-      if((number_press%3)!=0){
-          x=number_press/3+1;
-          y=number_press%3;
-      }
-      else {
-          x=number_press/3;
-          y=3;
-      }
-    }
-    else{
-      x=0;
-      y=0;
-    }
-    draw_Input(x,y);
   }
   else{
-    controllerF=1;
+    x=0;
+    y=0;
   }
-  u8g.setPrintPos(120,60);
-  u8g.print(numberP);
-}
+  draw_Input(x,y);  //display number 
+} 
+
 void runtest(int n){
     switch (n){
       case 1:{
@@ -256,18 +240,16 @@ void setup(){
   flag=1;   
   u8glibSet();      
   irrecv.enableIRIn();                           //enable IR revicer 
-  key[maxKey]=0;
 }
 void loop() {
-xxx=0;
+   updateF=0;
    if (irrecv.decode(&results)) {  
         controller=results.value;     //storage key id
-        xxx=1;
-        if(flag==1&&controller==HEXN[12]) flag=2;
-        else if(flag==1&&controller==HEXN[12]) flag=2;
-        else if(flag==1&&controller==HEXN[13]) flag=3;
-        else if(flag==3&&controller==HEXN[3])  flag=1;
-        else if(flag==4){delay(300);flag=1;} 
+        updateF=1;
+        if(flag==1&&controller==HEXN[12]) flag=2;     //if we are in page_1, and you press number"1", the next page is page_1_1
+        else if(flag==1&&controller==HEXN[13]) flag=3; //in page_1,pressed number "2", the next is page_1_2;
+        else if(flag==3&&controller==HEXN[3])  flag=1; //in page_1_2, pressed button "playback", the next is page_1
+        else if(flag==4){delay(300);flag=1;}    //in page_1_1_1,pressed button "play", the next is page_1
         irrecv.resume();  
   }
   runtest(flag);
